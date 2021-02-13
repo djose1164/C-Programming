e7aaaf81cac9cfafc9cc305a4d55d03fd00d64df
Jose S. Daniel <djose1164@gmail.com>
2021-02-12 23:02:47 -0400

    Fixed the code style.

--- src/Struct/Translater.c
+++ src/Struct/Translater.c
@@ -3,7 +3,7 @@
 
 #define MAX_LIMIT 50
 #define stdin stdin
-#define MAX 50 
+#define MAX 50
 
 struct Translater
 {
@@ -14,7 +14,6 @@ struct Translater
 
 struct Translater translater[MAX_LIMIT];
 
-
 void add_word();
 void print_word();
 void set_limit_value();
@@ -24,15 +23,12 @@ int main(int argc, char const *argv[])
 {
     char numbers[MAX];
     set_limit_value(translater);
-    
-    
 
     char word_option = 'N';
 
     printf("*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*\n");
     printf("Translater Spanish-English v1.0\n");
     printf("*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*\n");
-
 
     do
     {
@@ -52,17 +48,17 @@ int main(int argc, char const *argv[])
             case 1:
                 add_word();
                 break;
-            
+
             case 2:
                 print_word();
                 break;
-            
+
             default:
                 printf("Please, type a correct option!\n");
                 break;
             }
-            
-        }while (choise == 0);
+
+        } while (choise == 0);
 
         printf("Do you wanna continue? (Y/N) ");
 
@@ -75,8 +71,7 @@ int main(int argc, char const *argv[])
             break;
         }
 
-    }while (1);
-
+    } while (1);
 
     return 0;
 }
@@ -87,7 +82,8 @@ int main(int argc, char const *argv[])
  * will save in the ,<b>spanish_word</b> otherwise
  * if case is 2 so the word will save in <b>english_word</b></p>
 */
-void add_word() {
+void add_word()
+{
     short choise;
     char numbers[MAX];
 
@@ -100,7 +96,8 @@ void add_word() {
 
     for (size_t i = 0; i < MAX_LIMIT; i++)
     {
-        if (i != 0) {
+        if (i != 0)
+        {
             break;
         }
         switch (choise)
@@ -114,7 +111,6 @@ void add_word() {
 
             change_last_character(translater[i].spanish_word);
 
-
             translater[i].limit = i + 1;
 
             // Meaning the world in the another language
@@ -123,7 +119,7 @@ void add_word() {
             change_last_character(translater[i].english_word);
             fflush(stdin);
             break;
-            
+
             // English case
         case 2:
             printf("Type the word: \n");
@@ -141,14 +137,12 @@ void add_word() {
             fflush(stdin);
             change_last_character(translater[i].spanish_word);
             break;
-        
+
         default:
             printf("There are only 2 options! Please select correctly!");
             break;
         }
-
     }
-
 }
 
 /**
@@ -159,7 +153,8 @@ void add_word() {
  * Depending of the case this method will print
  * the word with its meaning in the another language.
 */
-void print_word() {
+void print_word()
+{
     short choice;
     char array[MAX_LIMIT];
     char search_word[MAX_LIMIT];
@@ -184,90 +179,83 @@ void print_word() {
              *      Spanish
              * Compare with the string's method when the two word are the same
              * and print out it with your another language meaning.
-            */ 
-           if ((strcmp(translater[i].spanish_word, search_word)) == 0)
-           {
-               printf(
-                    "Your word is %s and its meaning in English is %s .\n", 
-                    translater[i].spanish_word, translater[i].english_word
-               );
+            */
+            if ((strcmp(translater[i].spanish_word, search_word)) == 0)
+            {
+                printf(
+                    "Your word is %s and its meaning in English is %s .\n",
+                    translater[i].spanish_word, translater[i].english_word);
 
-               break;
-           }
-           else
-           {
-               printf("Make sure that you have the word that your seach already save!\n");
-               break;
-           }
-           
+                break;
+            }
+            else
+            {
+                printf("Make sure that you have the word that your seach already save!\n");
+                break;
+            }
         }
-    
+
         break;
-    
+
     case 2:
         printf("Type the word to search: ");
         fflush(stdin);
         fgets(search_word, MAX_LIMIT, stdin);
 
         change_last_character(search_word);
-    
+
         for (size_t j = 0; j < MAX_LIMIT; j++)
         {
             /**
              *      English
              * Compare with the string's method when the two word are the same
              * and print out it with your another language meaning.
-            */ 
-           if ((strcmp(translater[j].english_word, search_word)) == 0)
-           {
-               printf(
-                    "Your word is %s and its meaning in Spanish is %s .\n", 
-                    translater[j].english_word, translater[j].spanish_word
-               );
+            */
+            if ((strcmp(translater[j].english_word, search_word)) == 0)
+            {
+                printf(
+                    "Your word is %s and its meaning in Spanish is %s .\n",
+                    translater[j].english_word, translater[j].spanish_word);
 
-               break;
-           }
-           else
-           {
-               printf("Make sure that you have the word that your seach already save!\n");
-               break;
-           }
+                break;
+            }
+            else
+            {
+                printf("Make sure that you have the word that your seach already save!\n");
+                break;
+            }
         }
-            
+
         break;
-    
+
     default:
         printf("Type a correct number!");
         break;
     }
-    
-  
-
 }
 
-void change_last_character(char word[MAX_LIMIT]) {
+void change_last_character(char word[MAX_LIMIT])
+{
     for (size_t i = 0; i < MAX_LIMIT; i++)
     {
         if (word[i] == '\n')
         {
             word[i] = '\0';
         }
-        
+
         if (word[i] == '\n')
         {
             word[i] = '\0';
         }
-        
     }
-    
+
     printf("it works change_last_character\n");
 }
 
-void set_limit_value() {
+void set_limit_value()
+{
     for (size_t i = 0; i < MAX_LIMIT; i++)
     {
         translater[i].limit = 0;
     }
-    
-    
 }
