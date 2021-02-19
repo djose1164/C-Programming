9c0c07b27a7ca859a508d46bc7b64decc431e103
Jose S. Daniel <djose1164@gmail.com>
2021-02-19 17:37:23 -0400

    Dynamic Matrix added.

---
+++ src/Practices/dynamic_matrix.c
@@ -0,0 +1,78 @@
+// Create 2 dynamic matrix, asking for columns and rows by keyboard.
+// The second matrix has to be a copy of the first matrix.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int **matrix, **matrix2;
+    int rows, columns;
+
+    do
+    {
+        printf("Enter the numbers of rows: ");
+        scanf("%d", &rows);
+
+        printf("Enter the numbers of columns: ");
+        scanf("%d", &columns);
+    } while (rows < 1 || columns < 1);
+
+    // Creation of the first matrix.
+    matrix = malloc(rows * sizeof(int));
+    for (size_t i = 0; i < rows; i++)
+    {
+        matrix[i] = malloc(columns * sizeof(int));
+
+        if (!matrix[i])
+        {
+            fprintf(stderr, "Memory couldn't be allocated.\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            printf("Enter the numbers for the frist matrix at position[%zu, %zu]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Creation of the second matrix.
+    matrix2 = malloc(rows * sizeof(int));
+    for (size_t i = 0; i < rows; i++)
+    {
+        matrix2[i] = malloc(columns * sizeof(int));
+
+        if (!matrix2[i])
+        {
+            fprintf(stderr, "Memory couldn't be allocated.\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            matrix2[i][j] = matrix[i][j];
+        }
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            printf("%d ", matrix2[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    free(matrix);
+    free(matrix2);
+
+    return EXIT_SUCCESS;
+}
